04ee0fae5097cf6e656a8eda8fa18c93318a2077
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2020-08-02 16:12:47 +0530

    more change

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from polls.models import Question
 from django.template import loader
 from django.shortcuts import render
-from django.db import models
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -15,12 +15,10 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
+    except:
         raise Http404("Question doesn't exist f42c7f9c")
     return render(request, 'polls/detail.html', {'question': question})
-    #return HttpResponse(f'you are at {question_id}')
-
-
+    # return HttpResponse(f'you are at {question_id}')
 
 
 def results(request, question_id):
@@ -30,6 +28,7 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on  f42c7f9c question %s." % question_id)
+
 
 def owner(request):
     return render(request, 'polls/index.html')
