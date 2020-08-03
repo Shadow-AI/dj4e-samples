92e14884e59749cf9ae5ffc29902fc8725f633c2
Shadow-AI <53759750+Shadow-AI@users.noreply.github.com>
2020-08-04 00:42:41 +0530

    .

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
-from polls.models import Question
+from django.urls import reverse
+
+from polls.models import Question, Choice
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
@@ -15,19 +17,31 @@ def index(request):
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
+        choice = Choice.objects.all()
     except:
         raise Http404("Question doesn't exist f42c7f9c")
-    return render(request, 'polls/detail.html', {'question': question})
-    # return HttpResponse(f'you are at {question_id}')
+    return render(request, 'polls/detail.html', {'question': question, 'choice':choice})
 
 
 def results(request, question_id):
-    response = "You're looking at the results f42c7f9c  of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on  f42c7f9c question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        print(selected_choice)
+
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'No choice selected'
+        })
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
 def owner(request):
